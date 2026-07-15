7c4fe45581ff8983b0a8d592641e89e79933a774
William Colgan <colgan.william@gmail.com>
2026-07-15 11:12:16 -0400

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // GitHub project page: https://jweissmanlab.github.io/mela-website/
 export default defineConfig({
-  site: 'https://jweissmanlab.github.io',
-  base: '/mela-website',
+  site: 'https://mela.wi.mit.edu',
+  base: '/',
   integrations: [react()],
 });
